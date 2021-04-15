e1e2b7b6c95abdb303959bd8af89faa901201ace
jacobarchambault <jacobarchambault@toolbox>
2021-04-15 08:51:43 -0400

    Move population logic into its own class.

---
+++ RecursivePopulation/src/Population.java
@@ -0,0 +1,79 @@
+import java.util.Scanner;
+
+public class Population {
+
+	Scanner scanner;
+
+	Population(Scanner scanner) {
+		this.scanner = scanner;
+	}
+
+	/**
+	 * The displayPopulation method displays the* population table header and then
+	 * calls the recursive show Population method to display the daily populations.
+	 *
+	 * @param startingOrganisms The number of starting organisms in the population.
+	 * @param increase          The daily increase percentage.
+	 * @param days              The number of days the organisms will be left to
+	 *                          multiply.
+	 */
+	public static void displayPopulation(final double startingOrganisms, final double increase, final int days) {
+		System.out.println("Day\t\tOrganisms");
+		System.out.println("-----------------------------");
+		Population.showPopulation(1, days, startingOrganisms, increase);
+	}
+
+	static int days(final Scanner keyboard) {
+		System.out.print("Enter the number of days the organisms will multiply: ");
+		int days = keyboard.nextInt();
+		// Validate the input.
+		while (days < 1) {
+			System.out.print("Invalid. Enter 1 or more: ");
+			days = keyboard.nextInt();
+		}
+		return days;
+	}
+
+	static double dailyIncrease(final Scanner keyboard) {
+		System.out.print("Enter the daily increase: ");
+		double increase = keyboard.nextDouble();
+		// Validate the input.
+		while (increase < 0) {
+			System.out.print("Invalid. Enter a non-negative number: ");
+			increase = keyboard.nextDouble();
+		}
+		return increase;
+	}
+
+	static double startingNumber(final Scanner keyboard) {
+		System.out.print("Enter the starting number " + "organisms: ");
+		double starting = keyboard.nextDouble();
+		// Validate the input.
+		while (starting < 2) {
+			System.out.print("Invalid. Must be at least 2. " + "Re-enter: ");
+			starting = keyboard.nextDouble();
+		}
+		return starting;
+	}
+
+	/**
+	 * The showPopulation method displays the daily populations for a group of
+	 * organisms for a specified day, and then calls itself to display the data for
+	 * the rest of the days in a time period.
+	 *
+	 * @param dayNum        The day number.
+	 * @param days          The number of days in the time period.
+	 * @param organisms     The current number of organisms.
+	 * @param dailyIncrease The daily increase percentage.
+	 */
+	private static void showPopulation(final int dayNum, final int days, double organisms, final double dailyIncrease) {
+		if (dayNum < days) {
+			System.out.println(dayNum + "\t\t" + organisms);
+			organisms += organisms * dailyIncrease;
+			Population.showPopulation(dayNum + 1, days, organisms, dailyIncrease);
+		} else {
+			System.out.println(dayNum + "\t\t" + organisms);
+		}
+	}
+
+}
